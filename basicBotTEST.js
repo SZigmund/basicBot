d92083109e9d91a06e75d6576a0565fa82389148
SZigmund <szigmund@hotmail.com>
2015-01-27 16:46:23 -0500

    39

--- basicBotTEST.js
+++ basicBotTEST.js
@@ -1,4 +1,4 @@
-/** 38
+/** 39
  *Copyright 2014 Yemasthui
  *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
@@ -204,7 +204,7 @@
 
     var basicBot = {
         /*ZZZ: Updated Version*/
-        version: "2.1.4.00038",
+        version: "2.1.4.00039",
         status: false,
         name: "basicBot",
         loggedInID: null,
@@ -1220,7 +1220,12 @@
             console.log("TODO - STARTUP retrieveSettings");
             retrieveSettings();
             console.log("TODO - STARTUP retrieveFromStorage");
-            retrieveFromStorage();
+            try {
+              retrieveFromStorage();
+			}
+            catch(err) {
+               console.log("ERROR: " + err.message);
+            }
             console.log("TODO - STARTUP 1");
             window.bot = basicBot;
             basicBot.roomUtilities.updateBlacklists();
