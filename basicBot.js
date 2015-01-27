f6e1e603d490293ed04e503c2a94c7ad802bfc50
SZigmund <szigmund@hotmail.com>
2015-01-27 14:47:18 -0500

    Updated songstatistics message to use emoticons

--- basicBot.js
+++ basicBot.js
@@ -858,7 +858,8 @@
             if (typeof lastplay === 'undefined') return;
             if (basicBot.settings.songstats) {
                 if (typeof basicBot.chat.songstatistics === "undefined") {
-                    API.sendChat("/me " + lastplay.media.author + " - " + lastplay.media.title + ": " + lastplay.score.positive + "W/" + lastplay.score.grabs + "G/" + lastplay.score.negative + "M.")
+                    API.sendChat("[" + lastplay.media.author + " - " + lastplay.media.title + "] [:thumbsup: " + lastplay.score.positive + " :star: " + lastplay.score.grabs + " :thumbsdown: " + lastplay.score.negative + "]")
+                    //API.sendChat("/me " + lastplay.media.author + " - " + lastplay.media.title + ": " + lastplay.score.positive + "W/" + lastplay.score.grabs + "G/" + lastplay.score.negative + "M.")
                 }
                 else {
                     API.sendChat(subChat(basicBot.chat.songstatistics, {artist: lastplay.media.author, title: lastplay.media.title, woots: lastplay.score.positive, grabs: lastplay.score.grabs, mehs: lastplay.score.negative}))
