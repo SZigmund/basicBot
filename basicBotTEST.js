3d207a86e9f17f2ef766953357f4e24960143dfc
SZigmund <szigmund@hotmail.com>
2015-01-27 20:28:49 -0500

    40.9

--- basicBotTEST.js
+++ basicBotTEST.js
@@ -1,4 +1,4 @@
-/** 40.8
+/** 40.9
  *Copyright 2014 Yemasthui
  *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
@@ -209,7 +209,7 @@
 
     var basicBot = {
         /*ZZZ: Updated Version*/
-        version: "2.1.4.00040.8",
+        version: "2.1.4.00040.9",
         status: false,
         name: "basicBot",
         loggedInID: null,
@@ -3040,8 +3040,7 @@
                     if (msg.length === cmd.length) return;
                     console.log("echoCommand:4");
                     var msgContent = msg.substring(cmd.length + 2);
-		    API.sendChat(msgContent);
-                    console.log("echoCommand:5");
+		    return API.sendChat(msgContent);
                     }
                 catch(err) {
                     console.log("echoCommand:ERROR: " + err.message);
