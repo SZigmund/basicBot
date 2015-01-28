e9d13acfa36f9391091dab618fbd4bcc45e52d35
SZigmund <szigmund@hotmail.com>
2015-01-27 19:33:01 -0500

    40.5

--- basicBotTEST.js
+++ basicBotTEST.js
@@ -1,4 +1,4 @@
-/** 40.4
+/** 40.5
  *Copyright 2014 Yemasthui
  *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
@@ -168,7 +168,7 @@
         }
 		}
 		catch(err) {
-		   console.log("ERROR: " + err.message);
+		   console.log("retrieveFromStorage:ERROR: " + err.message);
 		}
 
     };
@@ -209,7 +209,7 @@
 
     var basicBot = {
         /*ZZZ: Updated Version*/
-        version: "2.1.4.00040.4",
+        version: "2.1.4.00040.5",
         status: false,
         name: "basicBot",
         loggedInID: null,
