203d5a327909eadcddeeeff0d9dfefce3390af25
SZigmund <szigmund@hotmail.com>
2015-01-27 19:43:19 -0500

    40.6

--- basicBotTEST.js
+++ basicBotTEST.js
@@ -1,4 +1,4 @@
-/** 40.5
+/** 40.6
  *Copyright 2014 Yemasthui
  *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
@@ -209,7 +209,7 @@
 
     var basicBot = {
         /*ZZZ: Updated Version*/
-        version: "2.1.4.00040.5",
+        version: "2.1.4.00040.6",
         status: false,
         name: "basicBot",
         loggedInID: null,
@@ -3016,7 +3016,7 @@
                 }
             },
 
-             versionCommand: {
+             versionCommand: {  //Added 01/27/2015 Zig
                 command: 'version',
                 rank: 'mod',
                 type: 'exact',
@@ -3025,6 +3025,24 @@
                 }
             },
 
+             echoCommand: {   //Added 01/27/2015 Zig
+                command: 'echo',
+                rank: 'manager',
+                type: 'exact',
+                functionality: function (chat, cmd) {
+                try{
+                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                    var msg = chat.message;
+                    if (msg.length === cmd.length) return;
+                        var msgContent = msg.substring(cmd.length + 2);
+			API.sendChat(msgContent);
+                    }
+                catch(err) {
+                    console.log("echoCommand:ERROR: " + err.message);
+		}
+                }
+            },
             websiteCommand: {
                 command: ['website','web'],
                 rank: 'user',
