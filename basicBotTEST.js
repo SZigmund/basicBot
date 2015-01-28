449a3f0e3507e8264f9a4910dbe3b7e358b9c8a2
SZigmund <szigmund@hotmail.com>
2015-01-27 19:03:48 -0500

    Added Woot Command

--- basicBotTEST.js
+++ basicBotTEST.js
@@ -1,4 +1,4 @@
-/** 40
+/** 40.1
  *Copyright 2014 Yemasthui
  *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
@@ -209,7 +209,7 @@
 
     var basicBot = {
         /*ZZZ: Updated Version*/
-        version: "2.1.4.00040",
+        version: "2.1.4.00040.1",
         status: false,
         name: "basicBot",
         loggedInID: null,
@@ -3035,8 +3035,27 @@
                     }
                 }
             },
+            wootCommand: {   //Added 01/27/2015 Zig
+            try
+            {
+            	command: 'woot',
+            	rank: 'user',
+            	type: 'exact',
+            	functionality: function (chat, cmd)                 {
+            		var votebutton = $(".button-vote-positive");
+            		if (votebutton.length > 0) {
+            			votebutton[0].click();
+            			API.sendChat("This song rocks");
+            		}
+            		/*$('#button-vote-positive').click();*/
+            	}
+            }
+            catch(err) {
+            	console.log("wootCommand:ERROR: " + err.message);
+            }
+            },
             //todoer
-             zigCommand: {
+             zigCommand: { //Added 01/27/2015 Zig
                 command: 'zig',
                 rank: 'mod',
                 type: 'exact',
