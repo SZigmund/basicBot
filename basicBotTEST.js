e8adcee620ee5ab1a0dffa9df64a84cee6098b5a
SZigmund <szigmund@hotmail.com>
2015-03-01 15:09:29 -0500

    Update basicBotTEST.js

--- basicBotTEST.js
+++ basicBotTEST.js
@@ -1,4 +1,4 @@
-/** version: 2.1.4.00021.02
+/** version: 2.1.4.00021.03
 (UPDATED -> Commits on Feb 10, 2015)
  Creator: Yemasthui
     var botCreator = "Matthew (Yemasthui)";
@@ -7,6 +7,7 @@
  *Copyright 2014 Yemasthui
  *Modifications (including forks) of the code to fit personal needs are allowed only for personal use and should refer back to the original source.
  *This software is not for profit, any extension, or unauthorised person providing this software is not authorised to be in a position of any monetary gain from this use of this software. Any and all money gained under the use of the software (which includes donations) must be passed on to the original author.
+ */
  */
 
 (function () {
@@ -190,7 +191,7 @@
 
     var basicBot = {
         /*ZZZ: Updated Version*/
-        version: "2.1.4.00021.02",
+        version: "2.1.4.00021.03",
         status: false,
         name: "basicBot",
         loggedInID: null,
@@ -3775,14 +3776,24 @@
                 rank: 'manager',
                 type: 'startsWith',
                 functionality: function (chat, cmd) {
-                    if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                    if (!basicBot.commands.executable(this.rank, chat)) return void (0);
-                    var msg = chat.message;
-                    if (msg.length === cmd.length) return API.sendChat(subChat(basicBot.chat.nouserspecified, {name: chat.un}));
-                    var whoisuser = msg.substr(cmd.length + 2);
-					console.log("whois: " + whoisuser);
-                    var user = basicBot.userUtilities.lookupUserName(whoisuser);
-                    if (typeof user !== 'undefined')  return API.sendChat(user.id);
+				    try {
+                        if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
+                        if (!basicBot.commands.executable(this.rank, chat)) return void (0);
+                        var msg = chat.message;
+                        if (msg.length === cmd.length) return API.sendChat(subChat(basicBot.chat.nouserspecified, {name: chat.un}));
+                        var whoisuser = msg.substr(cmd.length + 2);
+					    console.log("whois: " + whoisuser);
+                        var user = basicBot.userUtilities.lookupUserName(whoisuser);
+                        if (typeof user !== 'undefined')  {
+						    console.log("USER ID: ");
+						    console.log("USER ID: " + user.id);
+						    API.sendChat("USER ID: " + user.id);
+						}
+					    console.log("TYPE: " + typeof user);
+                    }
+					catch(err) {
+					    console.log("whoisCommand:ERROR: " + err.message);
+					}
                 }
             },
 			zigCommand: {   //Added 01/27/2015 Zig
@@ -3794,7 +3805,7 @@
 				        basicBot.roomUtilities.updateBlacklists();
                     }
 					catch(err) {
-					    console.log("grabCommand:ERROR: " + err.message);
+					    console.log("zigCommand:ERROR: " + err.message);
 					}
                 }
             },
