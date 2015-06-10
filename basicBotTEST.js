791c6699598b64b7b21d7060c0cb9060c0eab7e1
SZigmund <szigmund@hotmail.com>
2015-06-10 15:37:14 -0400

    Update basicBotTEST.js

--- basicBotTEST.js
+++ basicBotTEST.js
@@ -1,4 +1,4 @@
-/** version: 2.1.4.00043.12
+/** version: 2.1.4.00043.13
 
 (UPDATED -> Commits on Feb 10, 2015)
  Creator: Yemasthui
@@ -277,7 +277,7 @@ votes":{"songs":3,"tasty":0,"woot":0,"meh":0,"curate":0}
     var botMaintainer = "Benzi (Quoona)";
     var basicBot = {
         /*ZZZ: Updated Version*/
-        version: "2.1.4.00043.12",
+        version: "2.1.4.00043.13",
         status: false,
         name: "basicBot",
         loggedInID: null,
@@ -5213,36 +5213,26 @@ You're so fat, you could sell shade.
             banlistCommand: {
                 command: 'banlist',
                 rank: 'manager',
-                type: 'exact',
+                type: 'startsWith',
                 functionality: function (chat, cmd) {
                     try {
                         if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
                         if (!basicBot.commands.executable(this.rank, chat)) return void (0);
                         basicBot.roomUtilities.logNewBlacklistedSongs();
+						var keyword = "";
+                        if (msg.length > cmd.length) keyword = msg.substring(cmd.length + 2).toUpperCase();
+						basicBot.roomUtilities.logDebug("Keyword: " + keyword);
                         for (var i = 0; i < basicBot.room.newBlacklist.length; i++) {
                             var track = basicBot.room.newBlacklist[i];
-                            basicBot.roomUtilities.chatLog(track.mid + ": [" + track.author + " - " + track.title + "]");
+							var trackinfo = track.title.toUpperCase() + track.author.toUpperCase();
+							if (trackinfo.indexOf(keyword) > -1)
+                                basicBot.roomUtilities.chatLog(track.mid + ": [" + track.author + " - " + track.title + "]");
 						}
-                        for (var i = 0; i < basicBot.room.newBlacklist.length; i++) {
-                            var track = basicBot.room.newBlacklist[i];
-                            basicBot.roomUtilities.chatLog(track.mid + ": [" + track.author + " - " + track.title + "]");
-						}
-                        for (var i = 0; i < basicBot.room.newBlacklist.length; i++) {
-                            var track = basicBot.room.newBlacklist[i];
-                            basicBot.roomUtilities.chatLog(track.mid + ": [" + track.author + " - " + track.title + "]");
-						}
-                        for (var i = 0; i < basicBot.room.newBlacklist.length; i++) {
-                            var track = basicBot.room.newBlacklist[i];
-                            basicBot.roomUtilities.chatLog(track.mid + ": [" + track.author + " - " + track.title + "]");
-						}
-                        for (var i = 0; i < basicBot.room.newBlacklist.length; i++) {
-                            var track = basicBot.room.newBlacklist[i];
-                            basicBot.roomUtilities.chatLog(track.mid + ": [" + track.author + " - " + track.title + "]");
-						}
+						/*
                         for (var track in basicBot.room.newBlacklist) {
                             basicBot.roomUtilities.chatLog(track.mid + ": [" + track.author + " - " + track.title + "]");
                             basicBot.roomUtilities.logObject(track, "Track");
-                        }
+                        }*/
                     }
                     catch (err) { basicBot.roomUtilities.logException("banlist: " + err.message); }
                 }
